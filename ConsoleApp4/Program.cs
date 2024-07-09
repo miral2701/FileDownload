681f0d1c7e1fe899342cfae41b9e89963c851680
Mira Madani <miramadani2701@gmail.com>
2024-07-09 15:54:42 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Download);
+            t1.Start();
+            Console.ReadLine();
+        }
+        public static void Download()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string url = "https://en.wikipedia.org/wiki/Winter_white_dwarf_hamster#/media/File:PhodopusSungorus_1.jpg";
+                string destinationPath = @"C:\Users\admin\Desktop\1.zip";
+
+                try
+                {
+                    client.DownloadFile(url, destinationPath);
+                    Console.WriteLine("Файл успешно загружен.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при загрузке файла: {ex.Message}");
+                }
+            }
+        }
+    }
+}
